0d3b5862ca8253cfc927bfa16035717e4bb680c9
Pritam Ghosh <19pritamghosh93@gmail.com>
2020-08-19 16:55:31 +0530

    css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,7 @@ const routes: Routes = [
     // canActivate: [AuthGuardService],
     component: ContractComponent,
   },
+  { path: "", redirectTo: "contract", pathMatch: "full" },
 ];
 
 @NgModule({
